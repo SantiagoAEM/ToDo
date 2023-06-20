63b2b2a117d974ac420812d8db3a38448b70daef
Santiago Escutia <ricobit@gmail.com>
2023-06-20 09:20:31 -0600

    se corrigen errores se agrega animacion de carga

--- src/Components/TodosLoading.js
+++ src/Components/TodosLoading.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "../Css/loading.css"
 
 function TodosLoading() {
     return(
-        <p>Cargandoo...</p>
+
+<div class="container">
+    <div class="item item-1"></div>
+    <div class="item item-2"></div>
+    <div class="item item-3"></div>
+    <div class="item item-4"></div>
+   
+</div> 
     );
     
 }
